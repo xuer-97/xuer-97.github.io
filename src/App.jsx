58042992680c3d26ba6488d37e264379bba5c493
xuer-97 <xuer_97@outlook.com>
2021-09-16 11:20:13 +0700

    lunch

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import logo from "./img/logo.jpg";
 import profile from "./img/profile.jpg";
+import profile2 from "./img/profile2.jpg";
 
 function App() {
   return (
@@ -40,15 +41,15 @@ function App() {
       </header>
 
       <section id="home">
-        <h1>
-          Hello, I am <strong>Xuer</strong>{" "}
+        <h1 className="section-title section-title_intro">
+          Hello, I am <strong>Xuer</strong>
         </h1>
-        <p>Programmer</p>
+        <p className="section-subtitle section-subtitle_accent">Programmer</p>
         <img src={profile} alt="" />
       </section>
 
       <section id="services">
-        <h2>What I do</h2>
+        <h2 className="section-title">What I do</h2>
         <div className="services">
           <div className="service">
             <h3>Design + Development</h3>
@@ -78,14 +79,59 @@ function App() {
             </p>
           </div>
         </div>
-        <a href="#">see my work</a>
+        <a href="#mywork">see my work</a>
       </section>
 
       <section id="aboutme">
-        
+        <h2 className="section-title">Who I am</h2>
+        <p className="section-subtitle section-subtitle_accent">
+          Lorem ipsum dolor sit amet
+        </p>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>{" "}
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+        <img src={profile2} alt="" />
       </section>
 
-      <section id="mywork"></section>
+      <section id="mywork">
+        <h2 className="section-title">My work</h2>
+        <p className="section-subtitle">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+
+        <div className="portfolio">
+          <a href="#">
+            <img src="" alt="" />
+          </a>
+        </div>
+      </section>
+
+      <footer>
+        <a href="mailto:xuer_97@outlook.com">xuer_97@outlook.com</a>
+        <ul className="social">
+          <li>
+            <a href="#">github</a>
+          </li>
+          <li>
+            <a href="https://discordapp.com/users/887574262662176773"></a>
+            @xuer_97
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }
